40683443595c234c7d33eb94e106d1977bf8b811
Hiep Do Trong <dotronghiephd@gmail.com>
2023-12-25 22:19:55 +0700

    3.0 visualize like video and take photo each second

--- realsense.py
+++ realsense.py
@@ -17,7 +17,9 @@ cfg.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 
 # Bắt đầu pipeline
 pipe.start(cfg)
-count = 0
+
+# Khởi tạo biến để theo dõi thời gian
+start_time = time.time()
 
 try:
     while True:
@@ -33,23 +35,26 @@ try:
         color_image = np.asanyarray(color_frame.get_data())
         depth_image = np.asanyarray(depth_frame.get_data())
 
-        # Hiển thị và lưu ảnh
-        cv2.imshow("Color Image", color_image)
-        cv2.imshow("Depth Image", depth_image)
+        depth_cm = cv2.applyColorMap(cv2.convertScaleAbs(depth_image,
+                                        alpha = 0.5), cv2.COLORMAP_JET)
 
-        # Lưu ảnh vào thư mục
-        color_filename = os.path.join(output_folder, "color_images", f"color_{count}.png")
-        depth_filename = os.path.join(output_folder, "depth_images", f"depth_{count}.tiff")
+        gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 
-        # Lưu ảnh chiều sâu dưới định dạng TIFF
-        depth_image_pil = Image.fromarray(depth_image)
-        depth_image_pil.save(depth_filename, format="TIFF")
+        cv2.imshow('rgb', color_image)
+        cv2.imshow('depth', depth_cm)
 
-        cv2.imwrite(color_filename, color_image)
+        # Lưu ảnh vào thư mục mỗi giây
+        current_time = time.time()
+        elapsed_time = current_time - start_time
 
-        # Chờ 1 giây
-        time.sleep(1)
-        count+=1
+        if elapsed_time >= 1:
+            # Lưu ảnh chiều sâu dưới định dạng TIFF
+            depth_filename = os.path.join(output_folder, "depth_images", f"depth_{int(current_time)}.tiff")
+            depth_image_pil = Image.fromarray(depth_image)
+            depth_image_pil.save(depth_filename, format="TIFF")
+
+            # Reset thời gian bắt đầu
+            start_time = time.time()
 
         # Nếu người dùng nhấn phím ESC, thoát vòng lặp
         if cv2.waitKey(1) == 27:
